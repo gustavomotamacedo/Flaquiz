f5a844802a49f9d9efbf91169531161ff64ccda1
Gustavo Macedo <mota.macedo05@gmail.com>
2024-08-13 19:59:54 -0300

    Update India.java

--- app/src/main/java/com/gustavomacedo/flaquiz/India.java
+++ app/src/main/java/com/gustavomacedo/flaquiz/India.java
@@ -1,6 +1,9 @@
 package com.gustavomacedo.flaquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.gustavomacedo.flaquiz.models.Usuario;
+
 public class India extends AppCompatActivity {
+
+    private Usuario usuario;
+    private RadioGroup rgRespostas;
+    private TextView resposta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +29,13 @@ public class India extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Intent in = getIntent();
+        Bundle bd = in.getExtras();
+        if (bd != null) {
+            usuario.setNome(bd.getString("nome"));
+            usuario.setQtdCorretas(bd.getInt("corretas"));
+            usuario.setQtdIncorretas(bd.getInt("incorretas"));
+        }
     }
 }
