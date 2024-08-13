2be13e524a9823fbb22dc35a10d932edd3558948
Gustavo Macedo <mota.macedo05@gmail.com>
2024-08-13 19:54:02 -0300

    Create models.Usuario.java

---
+++ app/src/main/java/com/gustavomacedo/flaquiz/models/Usuario.java
@@ -0,0 +1,39 @@
+package com.gustavomacedo.flaquiz.models;
+
+public class Usuario {
+    private String nome;
+    private int qtdCorretas;
+    private int qtdIncorretas;
+
+    public Usuario() {}
+
+    public Usuario(String nome, int qtdCorretas, int qtdIncorretas) {
+        this.nome = nome;
+        this.qtdCorretas = qtdCorretas;
+        this.qtdIncorretas = qtdIncorretas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQtdCorretas() {
+        return qtdCorretas;
+    }
+
+    public void setQtdCorretas(int qtdCorretas) {
+        this.qtdCorretas = qtdCorretas;
+    }
+
+    public int getQtdIncorretas() {
+        return qtdIncorretas;
+    }
+
+    public void setQtdIncorretas(int qtdIncorretas) {
+        this.qtdIncorretas = qtdIncorretas;
+    }
+}
