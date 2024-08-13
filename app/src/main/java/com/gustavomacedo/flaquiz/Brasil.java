0957526dcd9f0e34ffe55cbb3cc01cc005352509
Gustavo Macedo <mota.macedo05@gmail.com>
2024-08-13 14:52:31 -0300

    Update Brasil.java

--- app/src/main/java/com/gustavomacedo/flaquiz/Brasil.java
+++ app/src/main/java/com/gustavomacedo/flaquiz/Brasil.java
@@ -8,7 +8,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class brasil extends AppCompatActivity {
+public class Brasil extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
